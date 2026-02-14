4a6e0a92e91caa1057249bd464083193c182ae0b
chamsoudine omar <chamsoudineo35@gmail.com>
2026-02-14 20:52:47 +0000

    ajout de table

---
+++ app/Models/Etudiant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant extends Model
+{
+    protected $fillable = ['id_Etd','Nom','Prenom','Datenaiss','Email',
+     'id_Etd',
+    ];
+}
